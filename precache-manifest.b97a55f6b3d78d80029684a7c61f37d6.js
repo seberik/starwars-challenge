6f0388596061357f8aacb5fdc376338a2ce30332
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-01 20:32:21 +0000

    Deploy seberik/starwars-challenge to github.com/seberik/starwars-challenge.git:gh-pages

---
+++ precache-manifest.b97a55f6b3d78d80029684a7c61f37d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea51ac62e30067ca14d1d1957769b87",
+    "url": "/starwars-challenge/index.html"
+  },
+  {
+    "revision": "5d01bcd7d5d18bcc1481",
+    "url": "/starwars-challenge/static/css/main.a1bc9aee.chunk.css"
+  },
+  {
+    "revision": "c6efdd50702ab6cfcb0c",
+    "url": "/starwars-challenge/static/js/2.d61728e6.chunk.js"
+  },
+  {
+    "revision": "5d01bcd7d5d18bcc1481",
+    "url": "/starwars-challenge/static/js/main.b57c6bf0.chunk.js"
+  },
+  {
+    "revision": "6fb74df996504d80ea31",
+    "url": "/starwars-challenge/static/js/runtime-main.59f63349.js"
+  },
+  {
+    "revision": "65421befd52bf6bac86d127b0ed78f5e",
+    "url": "/starwars-challenge/static/media/loader.65421bef.svg"
+  }
+]);
